e362edcb3547a3b2fc79f4295f59def15d95e089
Dan Coleman <danthedj11@gmail.com>
2021-05-20 20:34:32 +0100

    Check for existence of user before attempting creation, and throw error if there is an existing user

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -9,22 +9,62 @@ async function registerAccount(newAccountData)
 
     return new Promise(function(resolve, reject) {
 
-
         var hash = bcrypt.hashSync(newAccountData.password, 12);
 
         var userData = {
-            username: newAccountData.username,
+            username: newAccountData.username.toLowerCase(),
             name: newAccountData.name,
-            email: newAccountData.email,
+            email: newAccountData.email.toLowerCase(),
             password: hash
         };
-    
-        User.create(userData)
-        .then(data => {
-            resolve(data);
+
+        // Check there isn't an existing user with same email or username.
+        User.findAll({
+            where: {
+                [Op.or]: [
+                    {
+                        email: userData.email
+                    },
+                    {
+                        username: userData.username
+                    }
+                ]
+            }
+        }).then(function(res) {
+
+            if(res.length == 0)
+            {
+
+                User.create(userData)
+                .then(data => {
+                    resolve(data);
+                })
+                .catch(err => {
+                    reject(err.message || "Error occurred while creating the user account");
+                });
+
+            }
+            else
+            {
+
+                if(res[0].username == userData.username)
+                {
+
+                    reject('Username is already taken.');
+
+                }
+                else if(res[0].email == userData.email)
+                {
+
+                    reject('Email is already taken.');
+
+                }
+
+            }
+
         })
-        .catch(err => {
-            reject(err.message || "Error occurred while creating the user account");
+        .catch((err) => {
+            reject(err);
         });
 
     });
